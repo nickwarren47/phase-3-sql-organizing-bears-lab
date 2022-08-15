a461a43564b3b542b25a790dbf48f614722f2465
Nick Warren <nickwarren47@gmail.com>
2022-08-15 11:50:36 -0600

    commit tree a633201e30cf33e0a3365f212f085268c38003df

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears VALUES (1,'Mr. Chocolate', 17, 'M', 'brown','nice', 1); 
+INSERT INTO bears VALUES (2,'Rowdy', 7, 'M', 'black','ok', 1);
+INSERT INTO bears VALUES (3,'Tabitha', 4, 'F', 'white','nice', 1);
+INSERT INTO bears VALUES (4,'Sergeant Brown', 21, 'M', 'brown','meh', 0);
+INSERT INTO bears VALUES (5,'Melissa', 10, 'F', 'grey','mean', 1);
+INSERT INTO bears VALUES (6,'Grinch', 6, 'M', 'brown','very sweet', 1);
+INSERT INTO bears VALUES (7,'Wendy', 19, 'F', 'black','meh', 0);
+INSERT INTO bears VALUES (8,null, 1, 'F', 'white','super sweet', 1);
+
+
